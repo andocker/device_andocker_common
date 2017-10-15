3f07c10c9f030378fa21709687023be6deebbc95
You-Sheng Yang <vicamo@gmail.com>
2017-10-15 14:35:40 +0800

    docker: define image build configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,1 +1,20 @@
 BOARD_IS_CONTAINER := true
+
+BOARD_DOCKER_ENVVARS := \
+  INIT_KERNEL_CMDLINE \
+  $(empty)
+# See external/qemu/android/android-emu/android/main-kernel-parameters.cpp,
+# function emulator_getKernelParameters for more.
+BOARD_DOCKER_ENV_INIT_KERNEL_CMDLINE := \
+  qemu=1 \
+  androidboot.hardware=ranchu \
+  android.bootanim=0 \
+  qemu.gles=1 \
+  qemu.opengles.version=65536 \
+  $(empty)
+
+BOARD_DOCKER_VOLUMES := \
+  /data \
+  $(empty)
+
+BOARD_DOCKER_ENTRYPOINT := "/init"
